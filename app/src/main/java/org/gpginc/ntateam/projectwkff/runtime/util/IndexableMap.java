5667ddb4b6684d04dc343e02ed9406d2d7ba1acd
GPgabigob07 <gameplaysgabigob07@gmail.com>
2019-11-02 12:34:57 -0300

    Updating

---
+++ app/src/main/java/org/gpginc/ntateam/projectwkff/runtime/util/IndexableMap.java
@@ -0,0 +1,6 @@
+package org.gpginc.ntateam.projectwkff.runtime.util;
+
+import java.util.Map;
+
+public class IndexableMap implements Map {
+}
---
+++ app/src/main/java/org/gpginc/ntateam/projectwkff/ui/widget/adapters/_deloggerAdapter.java
@@ -0,0 +1,6 @@
+package org.gpginc.ntateam.projectwkff.ui.widget.adapters;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class _deloggerAdapter extends RecyclerView.Adapter {
+}
